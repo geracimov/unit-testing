e532b0be6d37c0c051e4998995b55e7263b4436f
codetmn5 <123@123.rt>
2018-07-07 12:45:11 +0500

    123

--- src/main/java/ru/sbrf/course/Calculator.java
+++ src/main/java/ru/sbrf/course/Calculator.java
@@ -5,5 +5,8 @@ public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+    public int mult(int a, int b) {
+        return a * b;
+    }
 
 }
--- src/test/java/ru/sbrf/course/CalculatorTest.java
+++ src/test/java/ru/sbrf/course/CalculatorTest.java
@@ -1,5 +1,6 @@
 package ru.sbrf.course;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -8,6 +9,7 @@ import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
 
@@ -26,14 +28,46 @@ public class CalculatorTest {
 
     private Calculator calculator = new Calculator();
 
-    @Parameters
+
+    @Parameters(name = "{index}: testMult[{0}, {1}] = {2}")
+    public static Iterable<Object[]> data1() {
+        return Arrays.asList(new Object[][]{
+                {5, 2, 10},
+                {1, 1, 1},
+                {2, 1, 2}
+        });
+    }
+
+    @Parameters(name = "{index}: testAdd({0}, {1}, {2})")
     public static Iterable<Object[]> data() {
-        return Arrays.asList(new Object[][]{{5, 2, 7}, {1, 1, 2}, {2, 1, 3}});
+        return Arrays.asList(new Object[][]{
+                {5, 2, 7},
+                {1, 1, 2},
+                {2, 1, 3}
+        });
     }
 
     @Test
     public void testAdd() throws Exception {
+        System.out.println("+++++++++++++++++");
         assertEquals(expected, calculator.add(a, b));
     }
+
+    @Test
+    public void testMult() throws Exception {
+        System.out.println("-----------------");
+        assertEquals(expected, calculator.mult(a, b));
+    }
+
+    private int aa;
+    private int bb;
+    private int ee;
+
+    public void testAdd( int a, int b, int e ) {
+        aa = a;
+        bb = b;
+        ee = e;
+    }
+
 
 }
